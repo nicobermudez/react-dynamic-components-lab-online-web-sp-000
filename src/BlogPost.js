480dcf3cbe8ff309c611ec1edcf9c003f629819c
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-24 15:56:37 +0000

    Automatically backed up by Learn

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Comments from './'
+import Comments from './Comment.js'
 // are we missing an import?
 
 export default class BlogPost extends Component {
