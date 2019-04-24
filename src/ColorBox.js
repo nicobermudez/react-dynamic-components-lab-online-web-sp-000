1df4bf61ce9def6734c7b969f99d8cd03c876c25
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-24 16:15:40 +0000

    Automatically backed up by Learn

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -5,7 +5,9 @@ export default class ColorBox extends Component {
   render() {
     return (
       <div className="color-box" style={{opacity: 2}}>
-        {/* your conditional code here! */}
+        {
+          
+        }
       </div>
     )
   }
